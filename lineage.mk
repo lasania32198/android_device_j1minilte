d0b415a22501bef8873fb327893491ab145da9bf
Your Name <you@example.com>
2024-09-14 11:01:52 +0200

    Added lineage make file.

---
+++ lineage.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := device/samsung/j105f
+
+# Inherit device configuration
+#TODO
+#$(call inherit-product, device/samsung/j1minilte/device_j1minilte.mk)
+
+PRODUCT_NAME := lineage_j1minilte
+PRODUCT_DEVICE := j1minilte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := J105F
+PRODUCT_MANUFACTURER := samsung
+
+ifneq ($(SIGN_BUILD),TRUE)
+PRODUCT_BUILD_PROP_OVERRIDES +=
+BUILD_FINGERPRINT=samsung/j1miniveltejv/j1minivelte:6.0.1/MMB29Q/J106FJVU0ARH1:user/test-keys \
+PRIVATE_BUILD_DESC="j1miniveltejv-user 6.0.1 MMB29Q J106FJVU0ARH1 release-keys"
+endif
