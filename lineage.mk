7817ed75b20950ce195673cf59849d9e2af58739
Your Name <you@example.com>
2024-09-15 13:08:26 +0200

    Added lineage make file

---
+++ lineage.mk
@@ -0,0 +1,27 @@
+# Release name
+PRODUCT_RELEASE_NAME := j1minivolte
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/gtexslte/full_j1minivolte.mk)
+
+# Use specific resolution for bootanimation
+TARGET_BOOTANIMATION_SIZE := 720x480
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j1minivolte
+PRODUCT_NAME := lineage_j1minivolte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-J105F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := phone
+
+TARGET_SCREEN_WIDTH := 480
+TARGET_SCREEN_HEIGHT := 800
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=j1minivolte \
+    BUILD_FINGERPRINT="samsung/j1miniveltejv/j1minivelte:6.0.1/MMB29Q/J106FJVU0ARH1:user/test-keys" \
+    PRIVATE_BUILD_DESC="j1miniveltejv-user 6.0.1 MMB29Q J106FJVU0ARH1 release-keys"
