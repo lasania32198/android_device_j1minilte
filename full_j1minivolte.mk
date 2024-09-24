e77a517a117c5bdd4c4702e09e38e7e23ace20cd
lasania32198 <153871883+lasania32198@users.noreply.github.com>
2024-09-24 10:33:44 +0200

    Delete full_j1minivolte.mk

--- full_j1minivolte.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, device/samsung/j1minivolte/device.mk)
-
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := j1minivolte
-PRODUCT_NAME := full_j1minivolte
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-J105F
-PRODUCT_MANUFACTURER := samsung
